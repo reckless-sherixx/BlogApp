36fa2c694df3a32fe15f16020d82b1b40bb93520
Vidyansh <ezboi2312@gmail.com>
2025-01-01 22:16:35 +0530

    recommit

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -20,7 +20,7 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
         <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
           <div className="col-span-8 pr-10">
             <div className="text-5xl font-extrabold">{blog.title}</div>
-            <div className="text-slate-500 pt-2">Posted on 2nd December 2024</div>
+            <div className="text-slate-500 pt-2">Posted on 1st Jan 2025</div>
             <div className="pt-4">{blog.content}</div>
           </div>
           <div className="col-span-4">
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,54 +1,40 @@
-import React, { useEffect, useState } from 'react';
 import { Appbar } from '../components/Appbar';
 import { BlogCard } from '../components/BlogCard';
 import { Skeleton } from '../components/Skeleton';
 import { useBlogs } from '../hooks';
 
 export const Blogs = () => {
-  const { loading, blogs } = useBlogs();
-  const [authorName, setAuthorName] = useState('');
+    const { loading, blogs } = useBlogs();
 
-  useEffect(() => {
-    const name = localStorage.getItem('name');
-    if (name) {
-      setAuthorName(name);
+    if (loading || !blogs) {
+        return <div>
+            <Appbar />
+            <div className='flex justify-center'>
+                <div>
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton />
+                </div>
+            </div>
+        </div>
     }
-  }, []);
 
-  if (loading || !blogs) {
     return (
-      <div>
-        <Appbar />
-        <div className='flex justify-center'>
-          <div>
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <Appbar />
-      <div className='flex justify-center'>
         <div>
-          {blogs.map(blog => (
-            <BlogCard
-              key={blog.id}
-              id={blog.id}
-              authorName={blog.author.name || authorName || 'Anonymous'}
-              title={blog.title}
-              content={blog.content}
-              publishedDate={"2nd Feb 2024"}
-            />
-          ))}
+            <Appbar />
+            <div className='flex justify-center'>
+                <div>
+                    {blogs.map(blog => <BlogCard
+                        id={blog.id}
+                        authorName={blog.author.name || "Anonymous"}
+                        title={blog.title}
+                        content={blog.content}
+                        publishedDate={"1st Jan 2025"}
+                    />)}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
